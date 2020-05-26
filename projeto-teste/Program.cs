ef006c78b624eaa624ef11f9f5b1788ebf7246bb
Isabela Macedo <isabelacb.macedo@gmail.com>
2020-05-26 20:12:56 -0300

    Modificada a frase

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -8,7 +8,7 @@ namespace projeto_teste
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Good Morning!");
-            Console.WriteLine("How are you today?");
+            Console.WriteLine("Have a nice day");
 
             Console.ReadLine();
         }
